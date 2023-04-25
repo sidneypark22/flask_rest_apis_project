1b7f47e937aebde9b2ea5115aba03fee675c4c3f
Sidney Park <spa0220@gmail.com>
2023-04-26 01:32:22 +1200

    Added method to enable deletion of item from html.

--- resources/web.py
+++ resources/web.py
@@ -1,6 +1,6 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-from flask import current_app, render_template, request
+from flask import current_app, render_template, request, redirect, url_for
 from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 from sqlalchemy import or_
 from passlib.hash import pbkdf2_sha256
@@ -26,10 +26,40 @@ def home():
     stores = StoreModel.query.order_by(StoreModel.name).all()
     return render_template('home.html', stores=stores)
 
-@blp.route('/home/item/<store_id>')
+@blp.route('/home/store/<store_id>/item/', methods=['GET', 'POST'])
 def item(store_id):
-    items = ItemModel.query.filter(ItemModel.store_id==store_id).order_by(ItemModel.price.asc())
-    return render_template('item.html', items=items)
+    if request.method == 'GET':
+        items = ItemModel.query.filter(ItemModel.store_id==store_id).order_by(ItemModel.price.asc())
+        return render_template('item.html', items=items, store_id=store_id)
+
+@blp.route('/home/store/<store_id>/item/delete/<item_id>', methods=['GET', 'POST'])
+def item_delete(store_id, item_id):
+    if request.method == 'POST':
+        item = ItemModel.query.get_or_404(item_id)
+        db.session.delete(item)
+        db.session.commit()
+        return redirect(url_for('Web.item', store_id=store_id))
+        #return render_template('index.html', user=url_for('Web.item', store_id=store_id))
+        #return redirect(request.url)
+
+#def get_items(store_id):
+#    items = ItemModel.query.filter(ItemModel.store_id==store_id).order_by(ItemModel.price.asc())
+#    return render_template('item.html', items=items)
+#def action():
+#    if request.method == 'DELETE':
+#        render_template('index.html', user=request.form)
+#def delete():
+#    if request.form['item_submit'] == 'Delete Item':
+#        item_id = request.form['id']
+#        print(item_id)
+#        print('YASSSSSS!!!!')
+#    #item = ItemModel.query.get_or_404(item_id)
+#    #db.session.delete(item)
+#    #db.session.commit()
+#    return render_template('item.html')
+
+#def delete_item(item_id):
+#    def delete():
 
 
 #PRODUCTS = {
